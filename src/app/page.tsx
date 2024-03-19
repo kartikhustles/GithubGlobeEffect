3e60026e4fc5333cd973582731b79ff471e63576
Kartik Agarwal <kartikhustles@gmail.com>
2024-03-19 16:39:41 +0530

    Globe Effect Created

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GlobeDemo } from "./component/globe/GlobeDemo";
+
+const page = () => {
+  return (
+    <div>
+      <GlobeDemo />
+    </div>
+  );
+};
+
+export default page;
